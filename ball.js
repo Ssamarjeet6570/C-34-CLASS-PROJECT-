18890498772d91794d476d62c62a6a7e8e267b48
Ssamarjeet6570 <73939771+Ssamarjeet6570@users.noreply.github.com>
2021-01-25 17:02:08 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,25 @@
+class Ball {
+    constructor(x, y, r, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y,r, options);
+        this.r = r;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(CENTER);
+        fill("red");
+        stroke("blue");
+        strokeWeight(7);
+        circle(0,0,this.r);
+        pop();
+      }
+}
